b7d8fe2b97e01d45fad271408bc962ce790c9048
Eric Kim <48634064+hyeonukim@users.noreply.github.com>
2023-12-19 03:27:15 -0800

    edit

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
@@ -11,13 +11,35 @@ const Earth = () => {
     <primitive 
       object={earth.scene} 
       scale={1.5} 
-      position-y={-1}
+      position-y={isMobile? -1:-1.25}
       rotation-y={0} />
   );
 };
 
 const EarthCanvas = () => {
-  
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);  
+
   return (
     <Canvas
       shadows
@@ -38,7 +60,7 @@ const EarthCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Earth />
+        <Earth isMobile={isMobile}/>
 
         <Preload all />
       </Suspense>
